9e4afd71abd6c31bb55b02167308d21a03ea19c9
mariambuzaladze <mariambuzaladze10@example.come>
2024-02-26 20:08:18 +0400

    fix unput functionality and styles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const input = document.getElementsByClassName("input")[0];
+const input = document.getElementsByClassName("range-input")[0];
 const pageviews = document.getElementsByClassName("pageviews")[0];
 
 const pay = document.getElementsByClassName("pay")[0];
@@ -7,57 +7,46 @@ const month = document.getElementsByClassName("month")[0];
 let oldPrice = Number(pay.textContent.split("$")[1]);
 let newPrice = oldPrice * 3;
 
+function renewPrice() {
+  oldPrice = Number(pay.textContent.split("$")[1]);
+  newPrice = oldPrice * 3;
+
+  if (!left) {
+    pay.textContent = `$${newPrice}.00`;
+  }
+}
+
 input.addEventListener("input", function () {
   let currentValue = Number(input.value);
+
+  input.style.background = `linear-gradient(to right, #a4f3eb ${currentValue}%, #ccc ${currentValue}%)`;
 
   if (currentValue >= 0 && currentValue < 25) {
     pageviews.textContent = "10K PAGEVIEWS";
     pay.textContent = "$8.00";
 
-    oldPrice = Number(pay.textContent.split("$")[1]);
-    newPrice = oldPrice * 3;
-    if (!left) {
-      pay.textContent = `$${newPrice}.00`;
-    }
+    renewPrice();
   } else if (currentValue >= 25 && currentValue < 50) {
     pageviews.textContent = "50K PAGEVIEWS";
     pay.textContent = "$12.00";
 
-    oldPrice = Number(pay.textContent.split("$")[1]);
-    newPrice = oldPrice * 3;
-    if (!left) {
-      pay.textContent = `$${newPrice}.00`;
-    }
+    renewPrice();
   } else if (currentValue >= 50 && currentValue < 75) {
     pageviews.textContent = "100K PAGEVIEWS";
     pay.textContent = "$16.00";
 
-    oldPrice = Number(pay.textContent.split("$")[1]);
-    newPrice = oldPrice * 3;
-    if (!left) {
-      pay.textContent = `$${newPrice}.00`;
-    }
+    renewPrice();
   } else if (currentValue >= 75 && currentValue < 100) {
     pageviews.textContent = "500K PAGEVIEWS";
     pay.textContent = "$24.00";
 
-    oldPrice = Number(pay.textContent.split("$")[1]);
-    newPrice = oldPrice * 3;
-    if (!left) {
-      pay.textContent = `$${newPrice}.00`;
-    }
+    renewPrice();
   } else {
     pageviews.textContent = "1M PAGEVIEWS";
     pay.textContent = "$36.00";
 
-    oldPrice = Number(pay.textContent.split("$")[1]);
-    newPrice = oldPrice * 3;
-    if (!left) {
-      pay.textContent = `$${newPrice}.00`;
-    }
+    renewPrice();
   }
-
-  console.log(currentValue);
 });
 
 ////////////////////////////////
